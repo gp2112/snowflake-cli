2302ce8068073da4bf664f8062af66ee7ede6d61
gp2112 <guircp@usp.br>
2022-03-01 18:19:51 -0300

    Save peers data on db to watch stats

--- snowflake.py
+++ snowflake.py
@@ -4,41 +4,105 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotInteractableException
 from datetime import datetime
+import requests
 import time
+import os
+import sys
+import asyncio
 
-print('Starting snowflake...')
+def logPeerIp(log: str):
+    l = log.replace('\n', ' ').split()[10:]
+    ip, port = None, None
+    if 'IP6' in l:
+        ip = l[l.index('IP6')+1].strip()
 
-options = Options()
-options.add_argument('--headless')
+    elif 'IP4' in l:
+        ip = l[l.index('IP4')+1].strip()
 
-d = DesiredCapabilities.CHROME
-d['goog:loggingPrefs'] = {'browser':'ALL'}
+    return ip
 
-driver = webdriver.Chrome(options=options, desired_capabilities=d)
-driver.get('https://snowflake.torproject.org/embed.html')
+def getPeerData(ip):
+    r = requests.get(f'http://ip-api.com/json/{ip}?fields=16393')
+    if r.status_code != 200:
+        return {}
 
-# try to click the button 10 times
-for i in range(10):
-    time.sleep(2)
-    try:
-        button = driver.find_element(by=By.CLASS_NAME, value='slider')
-        button.click()
-        print('connected!')
-        break
-    except ElementNotInteractableException:
-        pass
+    data = r.json()
+    if data.get('status') != 'success':
+        return {}
 
+    return r.json()
 
-while True:
-    try:
-        for entry in driver.get_log('browser'):
-            date = datetime.fromtimestamp(int(str(entry['timestamp'])[:-3])).strftime('%Y-%m-%d %H:%M:%S')
-            msg = entry['message'].split('"')[1].replace('\\r\\n', '\n')
-            print(f'{date} - {msg}')
+def parsePeer(msg, save=False):
+    ip = logPeerIp(msg)
+    p_data = getPeerData(ip)
+    print(f"{ip} from {p_data.get('region')}/{p_data.get('country')} is conected to you")
+    if save:
+        statsdb.savePeer(timestmp, ip, p_data.get('country'), p_data.get('region'))
+    return 0
 
-    except KeyboardInterrupt:
-        print('closing...')
-        button.click()
-        time.sleep(3)
-        driver.quit()
-        break
+async def main():
+
+    save_data = '--no-persist' not in sys.argv
+    if save_data: import statsdb
+
+    started_time = time.time()
+
+    print('Starting snowflake...')
+
+    if save_data and not os.path.isfile(statsdb.db_name):
+        print('Creating database...')
+        statsdb.createDb()
+        print(f'{statsdb.db_name} created!')
+
+    options = Options()
+    options.add_argument('--headless')
+
+    d = DesiredCapabilities.CHROME
+    d['goog:loggingPrefs'] = {'browser':'ALL'}
+
+    driver = webdriver.Chrome(options=options, desired_capabilities=d)
+    driver.get('https://snowflake.torproject.org/embed.html')
+
+    # try to click the button 10 times
+    for i in range(10):
+        time.sleep(2)
+        try:
+            button = driver.find_element(by=By.CLASS_NAME, value='slider')
+            button.click()
+            print('connected!')
+            break
+        except ElementNotInteractableException:
+            pass
+
+    peers_connected = 0
+
+    while True:
+        try:
+            current_time = time.time()
+
+            for entry in driver.get_log('browser'):
+                timestmp = int(str(entry['timestamp'])[:-3])
+                date = datetime.fromtimestamp(timestmp).strftime('%Y-%m-%d %H:%M:%S')
+                msg = entry['message'].split('"')[1].replace('\\r\\n', f'\n{date}')
+
+                print(f'{date} - {msg}')
+
+                if 'IP4' in msg or 'IP6' in msg:
+                    peers_connected += 1
+                    await asyncio.to_thread(parsePeer, msg, save=save_data)
+
+            if (current_time-started_time)%600==0:
+                print(f'\nYou have helped {peers_connected} people :)\n')
+
+        except KeyboardInterrupt:
+            print('\rclosing...')
+            time.sleep(3)
+            driver.quit()
+            break
+
+    if peers_connected > 0:
+        p = 'people' if peers_connected>1 else 'person'
+        print(f'You have helped {peers_connected} {p}!')
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ statsdb.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import sqlite3
+
+db_name = 'stats.db'
+
+def createDb():
+    with sqlite3.connect(db_name) as con:
+        cur = con.cursor()
+        cur.execute('CREATE TABLE peers (timestamp integer, ip text, country text, region text)')
+
+def savePeer(timestamp: int, ip: str, country, region):
+    print(f'Saving {ip} on database...')
+    with sqlite3.connect(db_name) as con:
+        cur = con.cursor()
+        cur.execute(f'''INSERT INTO peers
+            VALUES ({timestamp}, "{ip}", "{country}", "{region}")
+        ''')
+        con.commit()
+
+def getPeer(ip: str):
+    ip = ip.replace("'", '').replace('"', '') #no sql injection :)
+    with sqlite3.connect(db_name) as con:
+        cur = con.cursor()
+        cur.execute(f"SELECT * FROM peers WHERE ip='{ip}'")
+        data = cur.fetchall()
+    return data
+
+# receives date in isoformat or timestamp
+def getPeersByDate(date1, date2):
+    if type(date1) is str:
+        date1 = datetime.fromisofromat(date1).timestamp()
+    if type(date2) is str:
+        date2 = datetime.fromisofromat(date2).timestamp()
+
+    with sqlite3.connect(db_name) as con:
+        cur = con.cursor()
+        cur.execute(f'SELECT * FROM peers WHERE timestamp BETWEEN {date1} AND {date2}')
+        data = cur.fetchall()
+    return data
